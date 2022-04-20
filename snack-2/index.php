681039d753df73009b08c18322184740c18b7a9d
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-04-20 18:39:42 +0200

    finished

--- snack-2/index.php
+++ snack-2/index.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Snack 2</title>
+    <title>Snack-2</title>
 </head>
 <body>
     <?php if ((strlen($name) >= 3) && (strpos($mail, '@')) && (strpos($mail, '.')) && (is_numeric($age))) { ?>
